97cbacd0eff064a4cd7e637593b428b1ffd04d0b
Emily Landgrebe <emilandgrebe@gmail.com>
2019-12-02 16:04:16 -0600

    using while loops

--- lessons/loops.py
+++ lessons/loops.py
@@ -9,9 +9,23 @@ ninjas = ['ryu', 'crystal', 'yoshi', 'ken']
 #   print(ninja)
 
 # PRINTS 'RYU' 'CRYSTAL' AND 'YOSHI - BLACK BELT'
-for ninja in ninjas:
-  if ninja == 'yoshi':
-    print(f'{ninja} - black belt')
+# for ninja in ninjas:
+#   if ninja == 'yoshi':
+#     print(f'{ninja} - black belt')
+#     break
+#   else:
+#     print(ninja)
+
+
+age=25
+num=0
+
+while num<age:
+  if num==0:
+    num+=1
+    continue
+  if num%2==0:
+    print(num)
+  if num>10:
     break
-  else:
-    print(ninja)
+  num+=1
