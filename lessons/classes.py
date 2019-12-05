be4e938ab5b13e66661d165243e7dfb5a815ebcd
Emily Landgrebe <emilandgrebe@gmail.com>
2019-12-04 20:33:20 -0600

    init function creating new instances of a class

--- lessons/classes.py
+++ lessons/classes.py
@@ -1,19 +1,22 @@
 class Planet:
 
-  def __init__(self):
-    self.name='Hoth'
-    self.radius=200000
-    self.gravity=5.5
-    self.system='Hoth System'
+  def __init__(self,name,radius,gravity,system):
+    self.name = name
+    self.radius = radius
+    self.gravity = gravity
+    self.system = system
 
   def orbit(self):
     return f'{self.name} is orbiting in the {self.system}'
 
-hoth = Planet()
-
+hoth = Planet('Hoth',200000,5.5,'Hoth System')
 print(f'Name is: {hoth.name}')
 print(f'Radius is: {hoth.radius}')
 print(f'The Gravity is: {hoth.gravity}')
 print(hoth.orbit())
 
-planetX = Planet()
+naboo = Planet('Naboo',300000,8,'Naboo System')
+print(f'Name: {naboo.name}')
+print(f'Radius: {naboo.radius}')
+print(f'Gravity: {naboo.gravity}')
+print(naboo.orbit())
